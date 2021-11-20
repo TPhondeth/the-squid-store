15d2ef354aa144ae8885f7b1dbfd41670c3f56f1
lohith <lohith05amal@gmail.com>
2021-11-20 16:15:30 -0500

    navbar and hero

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-import { GiGiantSquid } from "react-icons/gi";
+import logo from "../../logo.png";
+import "./index.css";
+// import { GiGiantSquid } from "react-icons/gi";
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
+        <ul className="flex-row ">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
@@ -24,34 +23,36 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className=" nav-item active nav-item">
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li className="nav-item nav-item active">
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
       );
     }
   }
 
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span><GiGiantSquid /></span>
-          The Squid Store
-        </Link>
-      </h1>
-
-      <nav>
-        {showNavigation()}
-      </nav>
+    <header className="navbar  navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
+        <h1>
+          <Link to="/">
+            <span>
+              <img className="logo navbar-brand" src={logo} alt="" />
+            </span>
+          </Link>
+        </h1>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <nav className="navbar-item">{showNavigation()}</nav>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
