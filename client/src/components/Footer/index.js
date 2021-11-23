59fb3ee52e74c907c2ac2a00c24fb97644ae0407
Tony Phondeth <phondeth@gmail.com>
2021-11-23 12:31:35 -0500

    cleanup

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -19,35 +19,35 @@ const index = () => {
             <div className="d-flex">
               <div className="pt-3 text-center">
                 <FacebookShareButton
-                  url={"https://www.facebook.com/profile.php?id=100075389520028"}
-                  quote={"FullStack Developer"}
-                  hashtag="#MERN Stack Developer"
-                >
-                <FacebookIcon className="mx-3" size={36} />
+                    url={"https://www.facebook.com/profile.php?id=100075389520028"}
+                    quote={"FullStack Developer"}
+                    hashtag="#MERN Stack Developer"
+                  >
+                  <FacebookIcon className="mx-3" size={36} />
                 </FacebookShareButton>
   
                 <TwitterShareButton
-                  url={"https://twitter.com/SquidSq93709257"}
-                  quote={"FullStack Developer"}
-                  hashtag="#MERN Stack Developer"
-                >
-                <TwitterIcon className="mx-3" size={36} />
+                    url={"https://twitter.com/SquidSq93709257"}
+                    quote={"FullStack Developer"}
+                    hashtag="#MERN Stack Developer"
+                  >
+                  <TwitterIcon className="mx-3" size={36} />
                 </TwitterShareButton>
               
                 <LinkedinShareButton
-                  // url={"https://www.linkedin.com/in/"}
-                  quote={"FullStack Developer"}
-                  hashtag="#MERN Stack Developer"
-                >
-                <LinkedinIcon className="mx-3" size={36} />
+                    // url={"https://www.linkedin.com/in/"}
+                    quote={"FullStack Developer"}
+                    hashtag="#MERN Stack Developer"
+                  >
+                  <LinkedinIcon className="mx-3" size={36} />
                 </LinkedinShareButton>
               </div>
             </div>
           </div>
-            <div className="copyright">
-                Copyright&copy;
-                {new Date().getFullYear()}&nbsp;Squid Store
-            </div>
+              <div className="copyright">
+                  Copyright&copy;
+                  {new Date().getFullYear()}&nbsp;Squid Store
+              </div>
         </div>
       </div>
     </div>
