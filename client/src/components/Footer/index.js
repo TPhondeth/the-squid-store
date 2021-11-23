6497406d9ec402393d50e0175dafd4da41797c64
Tony Phondeth <phondeth@gmail.com>
2021-11-23 13:28:11 -0500

    cleaned up footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,49 +7,46 @@ import {
   TwitterShareButton,
   LinkedinIcon,
   LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton
 } from "react-share";
-import { Link } from "react-router-dom";
 
 const index = () => {
   return (
     <div className="footer ">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-6 col-sm-6">
-            <div className="d-flex">
-              <div className="pt-3 text-center">
-                <FacebookShareButton
-                    url={"https://www.facebook.com/profile.php?id=100075389520028"}
-                    quote={"FullStack Developer"}
-                    hashtag="#MERN Stack Developer"
-                  >
-                  <FacebookIcon className="mx-3" size={36} />
-                </FacebookShareButton>
-  
-                <TwitterShareButton
-                    url={"https://twitter.com/SquidSq93709257"}
-                    quote={"FullStack Developer"}
-                    hashtag="#MERN Stack Developer"
-                  >
-                  <TwitterIcon className="mx-3" size={36} />
-                </TwitterShareButton>
-              
-                <LinkedinShareButton
-                    // url={"https://www.linkedin.com/in/"}
-                    quote={"FullStack Developer"}
-                    hashtag="#MERN Stack Developer"
-                  >
-                  <LinkedinIcon className="mx-3" size={36} />
-                </LinkedinShareButton>
-              </div>
-            </div>
+          <div className="pt-3 text-center">
+            <FacebookShareButton
+                url={"https://www.facebook.com/profile.php?id=100075389520028"}
+                quote={"FullStack Developer"}
+                hashtag="#MERN Stack Developer"
+              >
+              <FacebookIcon className="mx-3" size={36} />
+            </FacebookShareButton>
+
+            <TwitterShareButton
+                url={"https://twitter.com/SquidSq93709257"}
+                quote={"FullStack Developer"}
+                hashtag="#MERN Stack Developer"
+              >
+              <TwitterIcon className="mx-3" size={36} />
+            </TwitterShareButton>
+          
+            <LinkedinShareButton
+                url={"https://www.linkedin.com"}
+                quote={"FullStack Developer"}
+                hashtag="#MERN Stack Developer"
+              >
+              <LinkedinIcon className="mx-3" size={36} />
+            </LinkedinShareButton>
+
+            <RedditShareButton
+                url={"https://www.reddit.com/"}
+                quote={"FullStack Developer"}
+                hashtag="#MERN Stack Developer"
+              >
+              <RedditIcon className="mx-3" size={36} />
+            </RedditShareButton>
           </div>
-              <div className="copyright">
-                  Copyright&copy;
-                  {new Date().getFullYear()}&nbsp;Squid Store
-              </div>
-        </div>
-      </div>
     </div>
   );
 };
